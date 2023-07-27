b1d85073068512038c23de4a26d38e5e94028a24
MinaMalak-cmd <engmina15@yahoo.com>
2023-07-27 14:06:14 +0300

    add tasks

--- src/modules/task/controllers/task.js
+++ src/modules/task/controllers/task.js
@@ -2,15 +2,26 @@ import taskModel from "../../../../DB/models/task.model.js";
 import { asyncHandler, SuccessResponse } from "../../../utils/errorHandling.js";
 
 export const addTask = asyncHandler(async (req, res, next) => {
-    const { title, description, status, deadline } = req.body;
-    const reqUser = req.user;
-    if (!["toDo", "doing", "done"].includes(status)) {
-        return next(new Error("Wrong value for status", { cause: 404 }));
-    }
+  const { title, description, status, deadline } = req.body;
+  const reqUser = req.user;
+  if (!["toDo", "doing", "done"].includes(status)) {
+    return next(new Error("Wrong value for status", { cause: 404 }));
+  }
 
-    const task = await taskModel.create({
-        title, description, status, deadline ,
-        userId: reqUser._id
-    });
-    return SuccessResponse(res, { message: "Done", task }, 201);
+  const task = await taskModel.create({
+    title,
+    description,
+    status,
+    deadline,
+    userId: reqUser._id,
   });
+  return SuccessResponse(res, { message: "Done", task }, 201);
+});
+
+export const getAllTasksWithUserData = asyncHandler(async (req, res, next) => {
+    const tasks = await taskModel.find({}).populate({
+        path: "userId",
+        select: "userName email phone",
+    });
+    return SuccessResponse(res, { message: "Done", tasks }, 200);
+});
--- src/modules/task/task.router.js
+++ src/modules/task/task.router.js
@@ -5,6 +5,7 @@ import handleAuth from '../../middlewares/handleAuth.js';
 const router = Router();
 
 router.post('/', handleAuth, taskController.addTask);
+router.get('/', taskController.getAllTasksWithUserData);
 // router.put('/update/:id', handleAuth, taskController.updateUser);
 // router.delete('/:id', handleAuth, taskController.deleteUser);
 // router.delete('/soft-delete/:id', handleAuth, taskController.softDelete);
