bd0a79c800fd46b2972ad42db24bea31b99402cb
MinaMalak-cmd <engmina15@yahoo.com>
2023-08-19 13:43:17 +0300

    improve multer locally

--- src/services/multerLocally.js
+++ src/services/multerLocally.js
@@ -1,48 +1,55 @@
 import multer from "multer";
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 import { allowedExtensions } from "../utils/allowedExtensions.js";
 
-export const multerUploadLocally = (allowedExtensionsArr, customPath) => {
-    if(!allowedExtensionsArr){
-        allowedExtensionsArr = allowedExtensions.Image;
-    }
-    if (!customPath) {
-        customPath = 'General'
-    }
-    const destPath = path.resolve(`uploads/${customPath}`);
+export const multerUploadLocally = (allowedExtensionsArr=allowedExtensions.Image, customPath="General") => {
 
-    //================================== Custom Path =============================
-    if(!fs.existsSync(destPath)) {
-        fs.mkdirSync(destPath, { recursive: true });
+  const destPath = path.resolve(`uploads/${customPath}`);
+
+  //================================== Custom Path =============================
+  if (!fs.existsSync(destPath)) {
+    fs.mkdirSync(destPath, { recursive: true });
+  }
+
+  //================================== Storage =============================
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, destPath);
+    },
+    filename: (req, file, cb) => {
+      const uniqueFileName =
+        Date.now() +
+        " " +
+        Math.round(Math.random() * 1e9) +
+        "-" +
+        file.originalname;
+      // or you can use nanoid library
+      // uniqueFileName = nanoid() + " " + file.originalname
+      file.finalDest = `uploads/${customPath}`;
+      // you must assign this value to the database instead of fle.path
+      // 34an mt3ml4 conversions backslashes to slash 3ady
+      cb(null, uniqueFileName);
+    },
+  });
+
+  //================================== File Filter =============================
+  const fileFilter = function (req, file, cb) {
+    if (allowedExtensionsArr.includes(file.mimetype)) {
+      return cb(null, true);
     }
+    cb(new Error("invalid extension", { cause: 400 }), false);
+  };
 
-    //================================== Storage =============================
-    const storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, destPath);
-        },
-        filename: function (req, file, cb) {
-            const uniqueFileName = Date.now() + " " + Math.round(Math.random() * 1e9) + "-" + file.originalname;
-            cb(null, uniqueFileName);
-        },
-    });
-
-    //================================== File Filter =============================
-    const fileFilter = function (req, file, cb) {
-        if (allowedExtensionsArr.includes(file.mimetype)) {
-          return cb(null, true)
-        }
-        cb(new Error('invalid extension', { cause: 400 }), false)
-      }
-    
-    const fileUpload = multer({
+  //you have to put fileFilter before storage 
+  // 34an tt check abl mt5zn
+  const fileUpload = multer({
     fileFilter,
     storage,
     // limits: {
     //   //   fields: 2,
     //   //   files: 2,
     // },
-    })
-    return fileUpload;
-}
+  });
+  return fileUpload;
+};
